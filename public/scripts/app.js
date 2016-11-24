e27cb70fa6d33057140b58c14a0aac9c47561530
Ryo <kanadajinlee@yahoo.ca>
2016-11-24 20:07:14 +0000

    OKR1 is finished, working on submit in OKR2

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,41 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
+  $.ajax({method: "GET", url: "/api/users"}).done((users) => {
+    for (user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
   });;
+  $("#add-url-btn").click(function(event) {
+    $("#url-input").focus();
+    $("#popup").slideToggle();
+  })
+
+/* When you click submit, this will send the data to the server */
+  $("add-url-form").on('submit', function(event) {
+    event.preventDefault();
+    renderResource();
+    // $.ajax({
+    //   url:,
+    //   success: function(text) {
+    //
+    //   }
+    // })
+  })
+
+
 });
+
+function renderResource() {
+  // cards.forEach(function(card)
+  $("#card-wall").preprend(createResource())
+}
+
+function createResource() {
+  let html = `<article class="card">
+    <h1> CARD~~~ </h1>
+    <p> URL: ... </p>
+    <p> Title: ... <p>
+    <p> Categories... </p>
+    <p> Likes and Ratings </p>
+    </article>`
+  return html;
+}
--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,7 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
+//just a placeholder...
   router.get("/", (req, res) => {
     knex
       .select("*")
