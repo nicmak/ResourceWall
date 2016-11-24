d369bcd6023c40d6a9298448d576bf07293daf57
nicmak <nicholas123mak@gmail.com>
2016-11-24 19:09:36 +0000

    Rough Work

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,18 @@
+function renderResource () {
+  $("#card-wall").preprend(createResource())
+}
+function createResource () {
+let html=
+`<article class="card">
+<h1> Card </h1>
+<p> URL:  ... </p>
+<p> Categories: ... </p>
+<p> Likes and Ratings </p>
+<p>
+</article>`
+  return html
+}
+
 $(() => {
   $.ajax({
     method: "GET",
@@ -6,5 +21,19 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+$("add-url-form").on('submit', function(event) {
+  event.preventDefault();
+
+  // $.ajax({
+  //   url:,
+  //   success: function(text) {
+  //
+  //   }
+  // })
+})
+
+
+
+
 });
