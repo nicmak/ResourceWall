87628ffb0beb46ef8e9e51bf523e846caec76949
Ryo <kanadajinlee@yahoo.ca>
2016-11-26 02:29:38 +0000

    Managed to make cards and users data from registration submission send to DB, but categories data is not properly being sent

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -24,58 +24,71 @@ function renderCards(cards) {
 };
 
 
-$(() => {
+$(document).ready(function() {
   $("#add-url-btn").click(function(event) {
     $("#url-input").focus();
     $("#popup").slideToggle();
   })
 
 /* When you click submit, this will send the data to the server */
-  $("#add-url-form").on('submit', function(event) {
-    event.preventDefault();
+  // $("#add-url-form").on('submit', function(event) {
+  //   event.preventDefault();
+  //
+  //   $.ajax({
+  //     url:"/api/users/cards",
+  //     method:"POST",
+  //     data: {
+  //       url: $("#url-input").val(),
+  //       categories: $("#categories-input").val(),
+  //       title: $("#title-input").val(),
+  //       notes: $("#notes-input").val()
+  //     }
+  //   })
+  //   .done(function(response) {
+  //     $(".result").append(JSON.stringify(response));
+  //     renderCards(response);
+  //     $('textarea').val("");
+  //     // loadCards();
+  //   });
+  //  })
 
-    $.ajax({
-      url:"/api/users/cards",
-      method:"POST",
-      data: {
-        url: $("#url-input").val(),
-        categories: $("#categories-input").val(),
-        title: $("#title-input").val(),
-        notes: $("#notes-input").val()
-      }
-    })
-    .done(function(response) {
-      $(".result").append(JSON.stringify(response));
-      renderCards(response);
-      $('textarea').val("");
-      // loadCards();
-    });
-   })
+  //  $("#registration-form").on('submit', function(event) {
+  //    event.preventDefault();
+   //
+  //    $.ajax({
+  //      url:"/api/users/registration",
+  //      method:"POST",
+  //      data: {
+  //        firstName: $("#first-name").val(),
+  //        lastName: $("#last-name").val(),
+  //        email: $("#email-registration").val(),
+  //        password: $("#password-registration").val()
+  //      }
+  //    })
+  //    .done(function(response) {
+  //       window.location="/user";
+   //
+  //    });
+  //  })
 
-   $("#registation-form").on('submit', function(event) {
-     event.preventDefault();
+    $("#categories-input").on('submit', function(event) {
+      event.preventDefault();
 
-     $.ajax({
-       url:"/api/users/registration",
-       method:"POST",
-       data: {
-         firstName: $("#first-name").val(),
-         lastName: $("#last-name").val(),
-         email: $("#email-registration").val(),
-         password: $("#password-registration").val()
-       }
+      $.ajax({
+        url:"/api/users/categories",
+        method:"POST",
+        data: {
+          category_name: $("#categories-input").val()
+        }
+      })
+      .done(function(response) {
+        console.log("cat cat cat")
+      })
      })
-     .done(function(response) {
-       $('#registration-form').val("");
-
-
-       // loadCards();
-     });
-    })
 
 
 
- })
+ });
 /* - - - - - - - - - - - - - - - - - - - - - - -*/
 function loadCards() {
   $.ajax({
@@ -96,13 +109,6 @@ function loadCards() {
   //     $('#data').val("");
   //     $(".counter").text("10");
   //   }
-
-
-
-
-
-
-
 
   // const urlText = $('#url-input').val();
 // console.log(urlText);
--- routes/users.js
+++ routes/users.js
@@ -20,24 +20,57 @@ module.exports = (knex) => {
     res.send("blah");
   });
 
-  router.post("/cards", (req,res) => {
+  // router.post("/cards", (req,res) => {
+  //   console.log("body:", req.body);
+  //   res.send(req.body);
+  //   knex('cards')
+  //     .insert({
+  //       url: req.body.url,
+  //       // categories: req.body.categories,
+  //       title: req.body.title,
+  //       notes: req.body.notes,
+  //       user_id: null //foreign key!
+  //   })
+  //   .then(function(rows) {
+  //     console.log("GREAT SUCCESS!!");
+  //   })
+  //   .catch(function(error){
+  //     console.log(error,"CATCH, MOTHAFUCKAAAA")
+  //   })
+  // });
+
+  // router.post("/registration", (req, res) => {
+  //   // res.redirect("/user");
+  //   res.send(req.body);
+  //   knex("users")
+  //     .insert({
+  //       first_name: req.body.firstName,
+  //       last_name:  req.body.lastName,
+  //       email: req.body.email,
+  //       password: req.body.password
+  //     })
+  //     .then(function(rows) {
+  //       console.log("GREAT SUCCESS!!");
+  //     })
+  //     .catch(function(error){
+  //       console.log(error,"CATCH, MOTHAFUCKAAAA")
+  //     })
+  // });
+
+  router.post("/categories", (req,res) => {
     console.log("body:", req.body);
     res.send(req.body);
-    knex('cards')
+    knex('categories')
       .insert({
-        url: req.body.url,
-        // categories: req.body.categories,
-        title: req.body.title,
-        notes: req.body.notes,
-        user_id: null //foreign key!
+        category_name: req.body.category_name,
+        card_id: null //foreign key!
     })
     .then(function(rows) {
-      console.log("GREAT SUCCESS!!");
+      console.log("GREAT CATEGORY !!");
     })
     .catch(function(error){
       console.log(error,"CATCH, MOTHAFUCKAAAA")
     })
   });
-
   return router;
 }
