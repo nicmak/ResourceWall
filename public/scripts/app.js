ba1add82c5011f6c4ede9cf8577f0bab0585d564
nicmak <nicholas123mak@gmail.com>
2016-11-26 18:36:36 +0000

    Able to insert information into category table now

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -16,16 +16,24 @@ function createCard(cardData) {
     </article>`
     return html;
 }
-
+//-----------------------------------------------
 function renderCards(cards) {
   // cards.forEach(card => {
     $("#card-wall").prepend(createCard(cards))
   // });
 };
+//-----------------------------------------------
 
-
-$(() => {
-    $("#add-url-btn").click(function(event) {
+function loadCards() {
+  $.ajax({
+    url:"/cards",
+    method: "GET",
+    success: function(response){
+      console.log(response);
+    }
+  })
+};
+//-----------------------------------------------
 
 $(document).ready(function() {
   $("#add-url-btn").click(function(event) {
@@ -46,12 +54,11 @@ $(document).ready(function() {
       }
     })
     .done(function(response) {
-      $(".result").append(JSON.stringify(response));
       renderCards(response);
       $('textarea').val("");
-      // loadCards();
     });
    })
+
    $("#registration-form").on('submit', function(event) {
      event.preventDefault();
      $.ajax({
@@ -68,91 +75,6 @@ $(document).ready(function() {
        $('#registration-form').val("");
      });
     })
- })
-  // $("#add-url-form").on('submit', function(event) {
-  //   event.preventDefault();
-  //
-  //   $.ajax({
-  //     url:"/api/users/cards",
-  //     method:"POST",
-  //     data: {
-  //       url: $("#url-input").val(),
-  //       categories: $("#categories-input").val(),
-  //       title: $("#title-input").val(),
-  //       notes: $("#notes-input").val()
-  //     }
-  //   })
-  //   .done(function(response) {
-  //     $(".result").append(JSON.stringify(response));
-  //     renderCards(response);
-  //     $('textarea').val("");
-  //     // loadCards();
-  //   });
-  //  })
 
-  //  $("#registration-form").on('submit', function(event) {
-  //    event.preventDefault();
-   //
-  //    $.ajax({
-  //      url:"/api/users/registration",
-  //      method:"POST",
-  //      data: {
-  //        firstName: $("#first-name").val(),
-  //        lastName: $("#last-name").val(),
-  //        email: $("#email-registration").val(),
-  //        password: $("#password-registration").val()
-  //      }
-  //    })
-  //    .done(function(response) {
-  //       window.location="/user";
-   //
-  //    });
-  //  })
-
-    $("#categories-input").on('submit', function(event) {
-      event.preventDefault();
-
-      $.ajax({
-        url:"/api/users/categories",
-        method:"POST",
-        data: {
-          category_name: $("#categories-input").val()
-        }
-      })
-      .done(function(response) {
-        console.log("cat cat cat")
-      })
-     })
-
-
-
- });
+}); //THIS IS WHERE FUNCTION READY ENDS....
 /* - - - - - - - - - - - - - - - - - - - - - - -*/
-function loadCards() {
-  $.ajax({
-    url:"/cards",
-    method: "GET",
-    success: function(response){
-      console.log(response);
-    }
-  })
-};
-  // event.preventDefault();
-  // $.ajax({ //this will send a post request to the server
-  //   url: '/tweets/',
-  //   method: 'POST',
-  //   data: $("#data").serialize(),
-  //   success: function(text) {
-  //     loadTweets();
-  //     $('#data').val("");
-  //     $(".counter").text("10");
-  //   }
-
-  // const urlText = $('#url-input').val();
-// console.log(urlText);
-    // $.ajax({
-    //   url:,
-    //   success: function(text) {
-    //
-    //   }
-    // })
--- routes/users.js
+++ routes/users.js
@@ -5,88 +5,63 @@ const router  = express.Router();
 
 module.exports = (knex) => {
 
-//just a placeholder...
-  router.get("/allCards", (req, res) => {
-    knex
-      .select("*")
-      .from("users")
-      .then((results) => {
-        res.json(results);
+router.get("/allCards", (req, res) => {
+  knex
+    .select("*")
+    .from("users")
+    .then((results) => {
+      res.json(results);
     });
-
-  });
-
-  router.get("/cards", (req,res) => {
-    res.send("blah");
-  });
+});
 
 //This will accept post requests that are /api/users + /cards
 //And then will execute the following code...
-  router.post("/cards", (req,res) => {
-    console.log("data:", req.data);
-    console.log("params:", req.params);
-    console.log("body:", req.body); //This console.log would be on your terminal
-    res.send(req.body);
-    knex
-     ('cards')
-     .insert({
-       url: req.body.url,
-       title:"Title",
-       notes:"Notes",
-       user_id:1})
+router.post("/cards", (req,res) => {
+  console.log("body:", req.body);
+  res.send(req.body);
+  knex('cards')
+    .insert({
+      url: req.body.url,
+      title: req.body.title,
+      notes: req.body.notes,
+      user_id: null //foreign key!
+  })
+  .then(function(response) {
+    console.log("GREAT Card!!");
+  })
+  .catch(function(error){
+    console.log(error,"CARD, MOTHAFUCKAAAA")
+  })
+  knex('categories')
+    .insert({
+      category_name: req.body.categories,
+      card_id: null
+  })
+  .then(function(response) {
+    console.log("GREAT CATEGORY !!");
+  })
+  .catch(function(error){
+    console.log(error,"CATEGORY, MOTHAFUCKAAAA")
+  })
+  //CAN I INCLUDE ANOTHER KNEX STATEMENT UNDERNEATH THAT WOULD RUN STIL;
+})
 
-
-  // router.post("/cards", (req,res) => {
-  //   console.log("body:", req.body);
-  //   res.send(req.body);
-  //   knex('cards')
-  //     .insert({
-  //       url: req.body.url,
-  //       // categories: req.body.categories,
-  //       title: req.body.title,
-  //       notes: req.body.notes,
-  //       user_id: null //foreign key!
-  //   })
-  //   .then(function(rows) {
-  //     console.log("GREAT SUCCESS!!");
-  //   })
-  //   .catch(function(error){
-  //     console.log(error,"CATCH, MOTHAFUCKAAAA")
-  //   })
-  // });
-
-  // router.post("/registration", (req, res) => {
-  //   // res.redirect("/user");
-  //   res.send(req.body);
-  //   knex("users")
-  //     .insert({
-  //       first_name: req.body.firstName,
-  //       last_name:  req.body.lastName,
-  //       email: req.body.email,
-  //       password: req.body.password
-  //     })
-  //     .then(function(rows) {
-  //       console.log("GREAT SUCCESS!!");
-  //     })
-  //     .catch(function(error){
-  //       console.log(error,"CATCH, MOTHAFUCKAAAA")
-  //     })
-  // });
-
-  router.post("/categories", (req,res) => {
-    console.log("body:", req.body);
-    res.send(req.body);
-    knex('categories')
-      .insert({
-        category_name: req.body.category_name,
-        card_id: null //foreign key!
+router.post("/registration", (req, res) => {
+  console.log("registration body", req.body);
+  res.send(req.body);
+  knex("users")
+    .insert({
+      first_name: req.body.firstName,
+      last_name:  req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
     })
     .then(function(rows) {
-      console.log("GREAT CATEGORY !!");
+      console.log("GREAT Registration!!");
     })
     .catch(function(error){
-      console.log(error,"CATCH, MOTHAFUCKAAAA")
+      console.log(error,"REGISTER, MOTHAFUCKAAAA")
     })
-  });
-  return router;
+});
+return router
 }
