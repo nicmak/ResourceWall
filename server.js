de94c546936e2def0e8a1cfcbf8d6b4408ccf08c
Ryo <kanadajinlee@yahoo.ca>
2016-11-27 02:30:32 +0000

    Cookie is able identified in USER TABLE, and SCRAPE is working able to get description

--- server.js
+++ server.js
@@ -13,12 +13,21 @@ const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
+const cookieSession = require('cookie-session')
+
 
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
+//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
+app.use(cookieSession({
+  name: 'session',
+  keys: ['lklkjahsdflkjhasdd', 'lklkjahsdflkjhasdf'],
+  maxAge: 60 * 60 * 1000 // 1 hour cookie life
+}))
+
 // The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan('dev'));
 
